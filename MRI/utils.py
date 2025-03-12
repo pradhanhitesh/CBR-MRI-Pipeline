b71c4c3cb640f7b21aa9e12774b000cc369b4fcb
pradhanhitesh <hiteshpradhan@iitgn.ac.in>
2025-03-12 14:12:06 +0530

    added compile func

--- MRI/utils.py
+++ MRI/utils.py
@@ -1,6 +1,11 @@
 from datetime import datetime
+from pydicom import dcmread
 import pytz
 import json
+import os
+import pandas as pd
+import magic
+
 
 def get_time():
     # Set timezone as GMT+05:30 IST
@@ -21,3 +26,49 @@ def read_json(json_path: str):
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError) as e:
         raise ValueError(f"Error reading JSON file {json_path}: {str(e)}")
+
+def extract_acquisition_times(files):
+    """
+    Extracts acquisition times from a list of DICOM files.
+
+    Parameters:
+        files (list): List of file paths.
+
+    Returns:
+        pd.DataFrame: DataFrame containing file paths and acquisition times.
+    """
+    dcm_time = []
+
+    for file in files:
+        if os.path.isdir(file):
+            continue
+
+        if magic.from_file(file, mime=True) == 'application/dicom':
+            try:
+                ds = dcmread(file)
+                acq_time = ds.get('AcquisitionTime', None)
+
+                if acq_time:
+                    dcm_time.append([file, acq_time])
+
+            except Exception as e:
+                print(f"Error reading {file}: {e}")
+
+    return pd.DataFrame(dcm_time, columns=['File', 'Time']).sort_values(by='Time')
+
+def compute_time_difference(time1: str, time2: str):
+    """
+    Computes the difference between two times given in HHMMSS.SSSSSS format.
+
+    Parameters:
+        time1 (str): Start time in the format 'HHMMSS.SSSSSS'
+        time2 (str): End time in the format 'HHMMSS.SSSSSS'
+
+    Returns:
+        str: Time difference in HH:MM:SS.SSSSSS format
+    """
+    time_format = "%H%M%S.%f"
+    t1 = datetime.strptime(time1, time_format)
+    t2 = datetime.strptime(time2, time_format)
+
+    return str(t2 - t1)
